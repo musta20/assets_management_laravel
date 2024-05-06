4ca227764a280eb4b2e261f4cbd199178e2986a3
musta20 <must.g20@gmail.com>
2024-05-06 13:46:22 +0300

    add Asset resources

---
+++ app/Filament/Resources/AssetResource.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AssetResource\Pages;
+use App\Filament\Resources\AssetResource\RelationManagers;
+use App\Models\Asset;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AssetResource extends Resource
+{
+    protected static ?string $model = Asset::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('category_id')
+                    ->required()
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('location_id')
+                    ->required()
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('vendor_id')
+                    ->required()
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('in_use'),
+                Forms\Components\DatePicker::make('purchase_date')
+                    ->required(),
+                Forms\Components\TextInput::make('item_type')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('physical'),
+                Forms\Components\TextInput::make('purchase_price')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('serial_number')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('warranty_information')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('depreciation_method')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('barcode')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('category_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('location_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('vendor_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('purchase_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('item_type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('purchase_price')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('serial_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('warranty_information')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('depreciation_method')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('barcode')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAssets::route('/'),
+            'create' => Pages\CreateAsset::route('/create'),
+            'edit' => Pages\EditAsset::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/AssetResource/Pages/EditAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\AssetResource\Pages;
+
+use App\Filament\Resources\AssetResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditAsset extends EditRecord
+{
+    protected static string $resource = AssetResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
