465c4d714d82c4c7467cca9c080d01e198e9db77
musta20 <must.g20@gmail.com>
2024-06-22 11:52:35 +0300

    fix vite and duster lint error

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
+    
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/css/filament/admin/theme.css',
+                'resources/js/app.js'
+            ],
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
